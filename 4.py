dc62839ee97499ef2b5401886a6f6e55045deebd
Karyakin2404 <dragunov24@mail.ru>
2021-01-10 15:24:42 +0300

    проверка

--- 4.py
+++
@@ -1,3 +0,0 @@
-string = input("Введите слова через пробелы - ").split()
-for i, word in enumerate(string, 1):
-    print(f'{i}, {word[:10]}')
--- 5.py
+++
@@ -1,9 +0,0 @@
-my_list = [9, 8, 7, 7, 7, 6, 5, 3, 3, 3, 2, 1]
-new_number = int(input('Введите новый элемент рейтинга в виде натурального числа - '))
-i = 0
-
-for n in my_list:
-    if new_number <= n:
-        i += 1
-my_list.insert(i, int(new_number))
-print(my_list)
